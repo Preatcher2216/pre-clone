9c2b2acaaf54eb45f0059fbef6e90c6ee02ad7b9
i.veshkin <i.veshkin@inostudio.com>
2023-04-25 13:34:40 +0300

    Merge vite template

--- client/src/store/users/selectors.ts
+++ client/src/store/users/selectors.ts
@@ -1,6 +1,6 @@
 import {createSelector} from '@reduxjs/toolkit';
 
-import {RootState} from 'store';
+import {RootState} from 'store/index';
 
 const selectUsers = createSelector(
   (state: RootState) => state.users.list,
